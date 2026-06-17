e20b78b92b6e70fb9e3cc86d1180b5b4cd9e282a
duongngocdoanh2606 <duongngocdoanh80@gmail.com>
2026-06-17 22:01:31 +0700

    10:01PM 17/6/2026

---
+++ QuanLyNhaHangDemo/Migrations/20260617150000_AddIsConfirmedToOrderModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyNhaHangDemo.Migrations
+{
+    public partial class AddIsConfirmedToOrderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "Orders",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "Orders");
+        }
+    }
+}
